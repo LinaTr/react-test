b4d9eb47eb4ca5172aa8636417f48a0d5ac68bfb
Polina <krugersha97@gmail.com>
2025-01-31 17:32:21 +0300

    add browser router

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-      <Dialogs />
-      {/* <Profile /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/dialogs" Component={Dialogs} />
+            <Route path="/profile" Component={Profile} />
+            <Route path="/news" Component={News} />
+            <Route path="/music" Component={Music} />
+            <Route path="/settings" Component={Settings} />
+          </Routes>
+        </div>
       </div>
-      
-      
-      
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,7 @@
+import s from "./Settings.module.css";
+
+function Settings(props) {
+  return <div>Settings</div>;
+}
+
+export default Settings;
