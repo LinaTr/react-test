d8713332e14a137407e7ae66a38a2d42a8448c44
Polina <krugersha97@gmail.com>
2025-02-01 15:48:39 +0300

    update Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,23 @@
 import s from "./Dialogs.module.css";
 
 function Dialogs(props) {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Mary</div>
+        <div className={s.dialog}>Kate</div>
+        <div className={s.dialog}>Alex</div>
+        <div className={`${s.dialog} ${s.active}`}>Eva</div>
+        <div className={s.dialog}>Natal</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Whats's your name?</div>
+        <div className={s.message}>How are you doing?</div>
+        <div className={s.message}>Where are you from?</div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
