61266e643457a5b58ee33744b6075d6893c2f307
Polina <krugersha97@gmail.com>
2025-02-19 15:43:35 +0300

    add onClick, ref in MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,26 +1,31 @@
+import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 function MyPosts(props) {
-
   let postsElements = props.posts.map((post) => (
     <Post message={post.message} likesCount={post.likesCount} />
   ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea name="" id=""></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        {postsElements}
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 }
